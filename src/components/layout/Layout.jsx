a7e724a721e7dcf1f6fc07870ae71f4c02e27e29
Dmitry Korochkin <abramslost@gmail.com>
2023-03-19 10:36:59 +0300

    add layout module

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,18 @@
-import Header from "./header/Header"
+import Header from './header/Header'
+import cn from 'clsx'
+import styles from './Layout.module.scss'
 
-const Layout = ({children}) => {
-  return (
-		<div>
-			<Header />
-			{children}
+const Layout = ({ children, bgImage, heading = '', backLink = '/' }) => {
+	return (
+		<div
+			className={cn(styles.wrapper, {
+				[styles.otherPage]: !!heading
+			})}
+			style={{ backgroundImage: `URL(${bgImage})` }}
+		>
+			<Header backLink={backLink}/>
+			{heading && <h1 className={styles.heading}>{heading}</h1>}
+			{children && <div>{children}</div>}
 		</div>
 	)
 }
