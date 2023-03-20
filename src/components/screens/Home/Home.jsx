74c30a994e075e762007199042b0baa8399d9145
Dmitry Korochkin <abramslost@gmail.com>
2023-03-20 21:25:57 +0300

    add Auth home

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,11 +1,19 @@
+import { useNavigate } from 'react-router-dom'
 import Layout from '../../layout/Layout'
+import Button from './../../ui/button/Button'
+import styles from './Home.modules.scss'
+import { useAuth } from './../../../hooks/useAuth'
 
 function Home() {
-	
+	const { isAuth } = useAuth()
 
+	const navigate = useNavigate()
 	return (
-		<Layout>
-			<div className='App'></div>
+		<Layout bgImage='/images/home-bg.jpg'>
+			<Button clickHandler={() => navigate(!isAuth ? '/new-workout' : '/auth')}>
+				{isAuth ? 'New' : 'Sign in'}
+			</Button>
+			<h1 className={styles.heading}>EXERCISES FOR THE SHOULDERS</h1>
 		</Layout>
 	)
 }
