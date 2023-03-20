07841c863a9b0cd1ed0d6299c2faa8a43b70bac7
Dmitry Korochkin <abramslost@gmail.com>
2023-03-20 20:24:27 +0300

    add menu link

--- src/components/layout/hamburger/Menu.jsx
+++ src/components/layout/hamburger/Menu.jsx
@@ -1,6 +1,7 @@
 import cn from 'clsx'
 import { menu } from './menu.data'
 import styles from './Hamburger.module.scss'
+import { Link } from 'react-router-dom'
 
 const Menu = ({ isShow }) => {
 	const logoutHandler = () => {}
@@ -13,8 +14,7 @@ const Menu = ({ isShow }) => {
 			<ul>
 				{menu.map((item, index) => (
 					<li key={`_menu_${index}`}>
-						{item.title}
-						{/* <Link to={item.link}>{item.title}</Link> */}
+						<Link to={item.link}>{item.title}</Link>
 					</li>
 				))}
 				<li>
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,23 +1,32 @@
 import { useAuth } from '../../../hooks/useAuth'
 import { AiOutlineArrowLeft } from 'react-icons/Ai'
+import { FaRegUser } from 'react-icons/Fa'
 import Hamburger from '../hamburger/Hamburger'
 import styles from './Header.module.scss'
-import { useLocation, useNavigation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
-const Header = ({ backLink }) => {
+const Header = ({ backLink = '' }) => {
 	const { pathname } = useLocation()
-	const navigate = useNavigation()
+	const navigate = useNavigate()
 
 	const { isAuth } = useAuth()
 	return (
 		<header className={styles.header}>
-			{pathname !== '/' && (
+			{pathname !== '/' ? (
 				<button
 					onClick={() => {
-						navigate(backLink || '/')
+						navigate(backLink)
 					}}
 				>
 					<AiOutlineArrowLeft />
+				</button>
+			) : (
+				<button
+					onClick={() => {
+						navigate('/profile')
+					}}
+				>
+					<FaRegUser fontSize={25}/>
 				</button>
 			)}
 			<Hamburger />
