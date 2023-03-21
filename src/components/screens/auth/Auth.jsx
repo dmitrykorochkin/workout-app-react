2891f83e1dc573f6723ba75adc8bfc0a60f36616
Dmitry Korochkin <abramslost@gmail.com>
2023-03-21 14:31:47 +0300

    install react-hooks-form

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,7 +1,12 @@
 
+import Layout from './../../layout/Layout';
 const Auth = () => {
   return (
-    <div>Auth</div>
+     <Layout heading="Sign in" bgImage='./images/auth-bg.png'>
+      <div className='wrapper-inner-page'>
+        <input type="text" placeholder='Enter email' />
+      </div>
+     </Layout>
   )
 }
 
--- src/routes/routes.data.js
+++ src/routes/routes.data.js
@@ -11,7 +11,7 @@ export const routes = [
   {
     path: "/auth",
     component: Auth,
-    isAuth: true
+    isAuth: false
   },
   {
     path: "/profile",
