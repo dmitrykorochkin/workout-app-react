f73fb9d455669a81e956eddd4f0bb6bdb25268d3
Dmitry Korochkin <abramslost@gmail.com>
2023-03-21 16:20:52 +0300

    add form

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,28 +1,33 @@
 import Layout from './../../layout/Layout'
 import { useForm } from 'react-hook-form'
 import Button from './../../ui/button/Button'
+import Field from './../../ui/field/Field'
 
 const Auth = () => {
-	const { register, handleSubmit, formState: { errors} } = useForm({
+	const {
+		register,
+		handleSubmit,
+		formState: { errors }
+	} = useForm({
 		mode: 'onChange'
 	})
 
-  const onSubmit = data => {
-    console.log(data)
-  }
+	const onSubmit = data => {
+		console.log(data)
+	}
 
 	return (
 		<Layout heading='Sign in' bgImage='./images/auth-bg.png'>
 			<div className='wrapper-inner-page'>
 				<form onSubmit={handleSubmit(onSubmit)}>
-					<input
+					<Field
+						error={errors?.email?.message}
+						name='email'
 						type='text'
+						register={register}
 						placeholder='Enter email'
-						{...register('email', {
-							required: 'Email is required'
-						})}
+						options={{ required: 'Email is required' }}
 					/>
-
 					<Button>Sign in</Button>
 				</form>
 			</div>
