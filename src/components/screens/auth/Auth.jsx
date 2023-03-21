3b67fe8a56622b9128cafdb2347e5974ee345e6d
Dmitry Korochkin <abramslost@gmail.com>
2023-03-21 14:59:09 +0300

    add submit in form

--- src/components/screens/auth/Auth.jsx
+++ src/components/screens/auth/Auth.jsx
@@ -1,13 +1,33 @@
+import Layout from './../../layout/Layout'
+import { useForm } from 'react-hook-form'
+import Button from './../../ui/button/Button'
 
-import Layout from './../../layout/Layout';
 const Auth = () => {
-  return (
-     <Layout heading="Sign in" bgImage='./images/auth-bg.png'>
-      <div className='wrapper-inner-page'>
-        <input type="text" placeholder='Enter email' />
-      </div>
-     </Layout>
-  )
+	const { register, handleSubmit } = useForm({
+		mode: 'onChange'
+	})
+
+  const onSubmit = data => {
+    console.log(data)
+  }
+
+	return (
+		<Layout heading='Sign in' bgImage='./images/auth-bg.png'>
+			<div className='wrapper-inner-page'>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input
+						type='text'
+						placeholder='Enter email'
+						{...register('email', {
+							required: 'Email is required'
+						})}
+					/>
+
+					<Button>Sign in</Button>
+				</form>
+			</div>
+		</Layout>
+	)
 }
 
 export default Auth
